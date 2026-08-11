03b19f9c8dfddf9d9b9022f66118000585f49f43
Amadiane <129948408+Amadiane@users.noreply.github.com>
2026-08-12 00:44:23 +0100

    projet front crée

--- Client/src/components/Routes/PrivateRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from 'react-router-dom';
-
-const PrivateRoute = () => {
-  const token = localStorage.getItem('access');
-  return token ? <Outlet /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,25 +1,7 @@
-/*import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})*/
-
-
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  json: {
-    namedExports: true,
-  },
-  server: {
-    hmr: {
-      overlay: false, // Désactive l'overlay d'erreur pour éviter les erreurs visuelles sur le navigateur
-    },
-  },
-});
+})
